8438725c3656c272c90f5de5d58be5b2fcde2db6
Guilherme Toledo <guilhermedesouzatoledo@gmail.com>
2023-02-12 12:37:10 +0100

    need help

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,21 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+        public void add(int value) {
+            if (this.size == 10) {
+                return;
+            }
+            this.grades[this.size] = value;
+            this.size++;
+
+    }
+    public int returnLast(int lastGrade) {
+        lastGrade = grades[grades.length - 1];
+        return lastGrade;
+
+}
+}
